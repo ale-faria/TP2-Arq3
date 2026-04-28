847748c6f47b594b7eb578dcedf7f16b76f97309
ale-faria <ale.faria.rodri@gmail.com>
2026-04-27 21:15:16 -0300

    ForwardingUnit

--- trabalho_pipeline/src/ForwardingUnit.v
+++ trabalho_pipeline/src/ForwardingUnit.v
@@ -1,35 +1,39 @@
 module ForwardingUnit (
     input  [4:0] idex_rs1,
     input  [4:0] idex_rs2,
-    input  [4:0] exmem_rd,
-    input  [4:0] memwb_rd,
 
+    input  [4:0] exmem_rd,
+    input        exmem_regwrite,
     input  [6:0] exmem_op,
+
+    input  [4:0] memwb_rd,
+    input        memwb_regwrite,
     input  [6:0] memwb_op,
 
     output reg [1:0] forwardA,
     output reg [1:0] forwardB
 );
 
-    localparam NO_FORWARD  = 2'b00;
-    localparam FROM_MEM    = 2'b01;
-    localparam FROM_WB_ALU = 2'b10;
-    localparam FROM_WB_LD  = 2'b11;
-
-    localparam LW    = 7'b000_0011;
-    localparam ALUop = 7'b001_0011;
-
-    initial begin
-      forwardA = NO_FORWARD;
-      forwardB = NO_FORWARD;
-    end
-
     always @(*) begin
-        forwardA = NO_FORWARD;
-        forwardB = NO_FORWARD;
+        // Padrão: sem forwarding
+        forwardA = 2'b00;
+        forwardB = 2'b00;
 
-        // TODO: implementar lógica do forwarding para operando A aqui!!!
-        // TODO: implementar lógica do forwarding para operando B aqui!!!
+        // Prioridade 1: dado mais recente vindo de EX/MEM
+        if (exmem_regwrite && (exmem_rd != 5'd0) && (exmem_rd == idex_rs1)) begin
+            forwardA = 2'b10;
+        end
+        else if (memwb_regwrite && (memwb_rd != 5'd0) && (memwb_rd == idex_rs1)) begin
+            forwardA = 2'b01;
+        end
+
+        // Prioridade 1: dado mais recente vindo de EX/MEM
+        if (exmem_regwrite && (exmem_rd != 5'd0) && (exmem_rd == idex_rs2)) begin
+            forwardB = 2'b10;
+        end
+        else if (memwb_regwrite && (memwb_rd != 5'd0) && (memwb_rd == idex_rs2)) begin
+            forwardB = 2'b01;
+        end
     end
 
 endmodule
